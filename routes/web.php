d86a1f9c9efeb826824943e5c20cc84f0ae3bfe7
GSoros <gsoros@gmail.com>
2017-01-27 11:58:48 +0100

    initial checkin

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+//Route::get('/', function () {
+//    return view('dashboard');
+//});
+
+Route::get('/', 'HomeController@dashboard');
