4f021f8bc14bc0c3146b868838ef2c0ae80bae58
GSoros <gsoros@gmail.com>
2017-04-13 23:19:50 +0200

    fix sql error on migration, see https://github.com/laravel/framework/issues/17508

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         DB::listen(function ($query) {
 
             $replace = function ($sql, $bindings)
