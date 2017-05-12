30c614dbb84b73532df74ff016055e23a9411cb2
GSoros <gsoros@gmail.com>
2017-05-12 16:37:29 +0200

    .

--- config/GTrader/TrainingManager.php
+++ config/GTrader/TrainingManager.php
@@ -1,8 +1,12 @@
 <?php
 
 return [
-    'slots'         => 2,       // number of concurrent training processes
-    'wait_for_slot' => 15,      // number of seconds to wait between checks for an open slot
+
+    // number of concurrent training processes
+    'slots'         => env('TRAINING_MANAGER_SLOTS', 2),
+
+    // number of seconds to wait between checks for an open slot
+    'wait_for_slot' => 15,
 
     // to run trainings with low process and io priority, you could add the following line to your .env
     // TRAINING_MANAGER_PHP_COMMAND="/usr/bin/ionice -c 3 /usr/bin/nice -n 19 /usr/bin/php"
