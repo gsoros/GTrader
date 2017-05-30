727e79fc6477329b3c7dc4afd1308731f85db468
GSoros <gsoros@gmail.com>
2017-05-30 14:02:45 +0200

    okcoin futures domain change

--- config/okcoin.php
+++ config/okcoin.php
@@ -2,7 +2,7 @@
 
 return [
     'domain'            => env('OKCOIN_DOMAIN', 'www.okcoin.com'),
-    'futures_domain'    => env('OKCOIN_FUTURES_DOMAIN', 'www.okcoin.com'),
+    'futures_domain'    => env('OKCOIN_FUTURES_DOMAIN', 'www.okex.com'),
     'api_version'       => env('OKCOIN_API_VERSION',  'v1'),
     'api_key'           => env('OKCOIN_API_KEY',       ''),
     'secret_key'        => env('OKCOIN_SECRET_KEY',    ''),
