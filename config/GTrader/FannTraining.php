f07c5d554c4ec1c7af3a524b4083fb5bf5d5818e
GSoros <gsoros@gmail.com>
2017-06-20 12:41:37 +0200

    .

--- config/GTrader/FannTraining.php
+++ config/GTrader/FannTraining.php
@@ -5,6 +5,9 @@ return [
     'train_range'           => ['start_percent' => 0,   'end_percent' => 30],
     'test_range'            => ['start_percent' => 30,  'end_percent' => 60],
     'verify_range'          => ['start_percent' => 60,  'end_percent' => 80],
+    'crosstrain'            => 0,
+    'reset_after'           => 0,
+    'maximize_for'          => 'Balance Fixed',
     'suffix'                => '.train',
     'max_boredom'           => 10,   // increase jump size after this number of uneventful epochs
     'epoch_jump_max'        => 100,  // max amount of epochs between tests
