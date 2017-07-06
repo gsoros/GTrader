dcb56bce7ad70e7488f346d9ba9ed34a555600e8
GSoros <gsoros@gmail.com>
2017-07-06 11:03:53 +0200

    Fuck. Let's attract some bastards from bloody shite commitlogsfromlastnight. Motherfuckers.

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,6 @@ if (\Config::get('app.env') === 'local') {
     Route::get('/phpinfo', function() {
         ob_start();
         phpinfo();
-        //dump(gd_info());
-        //echo 'GDFONTPATH='.getenv('GDFONTPATH');
         $info = ob_get_contents();
         ob_end_clean();
         return $info;
