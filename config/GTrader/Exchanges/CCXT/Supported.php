99fa53a75d94331099b414ee449c90ccd8525b48
G Soros <gsoros@users.noreply.github.com>
2019-11-10 17:02:11 +0100

    17f65494bcb20738a367d9578855d35f6ac512674a1762dd7946

--- config/GTrader/Exchanges/CCXT/Supported.php
+++ config/GTrader/Exchanges/CCXT/Supported.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'min_position_change' => 5,  // do not create orders if position change is less than this percentage
+    'min_position_change' => 1,  // do not create orders if position change is less than this percentage
 ];
---
+++ config/GTrader/Exchanges/CCXT/okex3.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'min_position_change' => 20,  // do not create orders if position change is less than this percentage
+];
